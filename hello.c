ca094725a226fa464898425305b32b742cfa9346
Carter Haberling <chaberling2@huskers.unl.edu>
2026-08-27 17:40:23 -0500

    fixed standard library bug

--- hello.c
+++ hello.c
@@ -1,7 +1,13 @@
 /**
+ * Carter Haberling
+ * 8/27/2026
+ *
  * Basic Hello World Program
  */
 
+ #include <stdlib.h>
+ #include <stdio.h>
+ 
  int main(int argc, char **argv){
 
     printf("Hello, I'm Carter Haberling!\n");
